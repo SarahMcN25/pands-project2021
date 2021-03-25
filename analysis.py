1d4008ffabbbb60009a1bacd9c4c144541d3bc6e
Sarah McNelis <G00398343@gmit.ie>
2021-03-25 21:02:35 +0000

    project

--- analysis.py
+++ analysis.py
@@ -1,4 +1,53 @@
-# This program is for analysis of Fisher's Iris sata set
+# This is program which analysis' Fisher's Iris data set
 # Author: Sarah McNelis
 
-#initial submit to check git repository works
+# PROGRESS ON PROJECT
+# MAPPED A PLAN OF HOW PROG SHOULD MAYBE GO?
+# NEXT STEP WRITING CODE!!! 
+
+#download iris data set
+#https://www.kaggle.com/saurabh00007/iriscsv
+
+# import modeules, numpy, malplotlib, 
+
+##### 1
+# open csv file here for reading
+# pandas here? view extra lecture to check
+filename = 
+with open(filename) as f: #default as reading mode
+    f.read()
+
+# to view column headings
+# https://towardsdatascience.com/a-beginners-guide-to-data-analysis-in-python-188706df5447
+# fyi sepal is green part of flower/bud after steam before petals 
+df.head()
+
+####### 2 VARIABLES
+# how many variables?
+# var 1 = how many species? 3... would look good on scatter plot ### count()
+
+# var 2 = average/mean of first 10? or las 10? or random 10? of first 10 petal lenghts ?
+#               maybe vs sepal lenghts? and maybe same for widths??
+#               is this possible sarah????
+#               https://www.w3schools.com/python/module_statistics.asp
+#               statistics.mean()	Calculates the mean (average) of the given data
+
+
+# var 3 = sum of widhts/ lenghts?
+#           math.fsum()	Returns the sum of all items in any iterable (tuples, arrays, lists, etc.) 
+#           https://www.w3schools.com/python/module_math.asp 
+
+# var 4 = lowest value of a list min()
+#               import math
+
+# var 5 = highest value of a list max()
+
+######### 3 
+# OUTPUT SUMMERARY OF VAR 1-5 INTO ONE TXT FILE
+
+#########4
+# histo, scatter and text file summary for each variable. 
+#var1-5 histo each 
+
+######### 5 
+#var 1-5 scatter each
