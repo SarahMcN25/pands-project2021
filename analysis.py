02b486cf96deab43131395300b55a20415147b54
Sarah McNelis <G00398343@gmit.ie>
2021-04-01 21:06:40 +0100

    project

--- analysis.py
+++ analysis.py
@@ -13,3 +13,19 @@ df = pd.read_table(filename) #open the file and read the table
 #print(df.tail(7))
 #print(df.shape) #prints shape of data set
 print(df.describe()) #gives brief analysis
+
+# to get mean of column
+meanvalues = df.groupby('sepellenght').mean()
+print(meanvalues)
+
+sumvalues = df.groupby('sepellenght').sum()
+print(sumvalues)
+
+minvalues = df.groupby('sepellenght').min()
+print(minvalues)
+
+maxvalues = df.groupby('sepellenght').max()
+print(maxvalues)
+
+# maybe create a function for each - mean, sum, min etc and then call function for each column???
+# might be neater and less code???? Think about it! 
