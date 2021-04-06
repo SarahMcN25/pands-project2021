9f2847d5faa1450997d24a6a390248d076b7a8af
Sarah McNelis <G00398343@gmit.ie>
2021-04-06 12:44:25 +0100

    project

--- analysis.py
+++ analysis.py
@@ -20,27 +20,31 @@ df = pd.read_csv(filename) #open and read csv file
 
 # var 1 = how many species - do scatter plot for this
 species = df['Species'].value_counts()  #gives count of each species
-#print(species)                          # <<https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.value_counts.html>>
+print(species)                          # <<https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.value_counts.html>>
 
 # var 2 - Mean of all columns 
 # <<https://www.statology.org/mean-of-column-pandas/>>
 # <<https://www.w3schools.com/python/module_math.asp>>
 meanvalues = df.mean()
-#print(meanvalues)
+print(meanvalues)
 
 # var 3 - sum of each column ### need to figure out how to omit the species column for these
 #sum of widhts/ lenghts?
 sumvalues = df.sum() 
-#print(sumvalues)
+print(sumvalues)
 
 # var 4 - lowest value for each column
 minvalues = df.min()
-#print(minvalues)
+print(minvalues)
 
 # var 5 - highest value for each column
 maxvalues = df.max()
-#print(maxvalues)
+print(maxvalues)
+
+x = minvalues
+y = maxvalues
+
 
 # 'Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width'
-df.plot.scatter(x= meanvalues, y=sumvalues)
+df.plot.scatter(meanvalues, maxvalues)
 plt.show()
